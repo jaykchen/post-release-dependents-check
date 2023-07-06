652a9d074f53b11f3f218458ed38a09c591605d8
Ji Chen <jaykchen@gmail.com>
2023-07-06 17:14:25 -0400

    clean up code, add comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
+use dotenv::dotenv;
 use flowsnet_platform_sdk::write_error_log;
 use github_flows::{
     get_octo, listen_to_event, octocrab::models::events::payload::WorkflowRunEventAction,
     EventPayload, GithubLogin,
 };
-use dotenv::dotenv;
 use std::env;
+
+
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
     dotenv().ok();
-    let github_owner = env::var("github_owner").unwrap_or("jaykchen".to_string());
-    let github_repo = env::var("github_repo").unwrap_or("chatgpt-private-test".to_string());
+    let github_owner = env::var("github_owner").unwrap_or("WasmEdge".to_string());
+    let github_repo = env::var("github_repo").unwrap_or("WasmEdge".to_string());
 
     listen_to_event(
         &GithubLogin::Default,
@@ -24,27 +26,35 @@ pub async fn run() {
 
 async fn handler(payload: EventPayload) {
     let octocrab = get_octo(&GithubLogin::Default);
-    // // repos to watch
-    // // vec of (owner, repo, workflow_id, branch)
-    // let owner_repo_workflow_id_branch_vec = vec![
-    //     ("second-state", "microservice-rust-mysql", "ci.yml", "main"),
-    //     ("second-state", "wasmedge-quickjs", "examples.yml", "main"),
-    // ];
-
-    // https://github.com/jaykchen/hacker-news-lambda/blob/main/.github/workflows/placeholder.yml
-    let owner_repo_workflow_id_branch_vec =
-        vec![("jaykchen", "hacker-news-lambda", "placeholder.yml", "main")];
+    // repos to watch
+    // vec of (owner, repo, workflow_id, branch)
+    // examples of workflow_run action files in their respective repositories:
+    // https://github.com/second-state/microservice-rust-mysql/blob/main/.github/workflows/ci.yml
+    // https://github.com/second-state/wasmedge-quickjs/blob/main/.github/workflows/examples.yml
+    let owner_repo_workflow_id_branch_vec = vec![
+        ("second-state", "microservice-rust-mysql", "ci.yml", "main"),
+        ("second-state", "wasmedge-quickjs", "examples.yml", "main"),
+    ];
 
     let _success = "success".to_string();
     match payload {
         EventPayload::WorkflowRunEvent(e) => {
+            // check the status of the upstream workflow_run is completed
             if e.action == WorkflowRunEventAction::Completed {
                 let workflow_run = e.workflow_run;
+                // check the result of the upstream workflow_run is success
                 match workflow_run.conclusion {
                     Some(_success) => {
-                        // https://github.com/second-state/microservice-rust-mysql/blob/main/.github/workflows/ci.yml
-                        // https://github.com/second-state/wasmedge-quickjs/blob/main/.github/workflows/examples.yml
-
+                        // to be triggered by this flows function, the downstream repo needs to have 
+                        // this block in its workflow action file:
+                        //```yml
+                        // workflow_dispatch:
+                        //   inputs:
+                        //     logLevel:
+                        //       description: 'Log level'
+                        //       required: true
+                        //       default: 'info'
+                        //```                  
                         for (owner, repo, workflow_id, branch) in owner_repo_workflow_id_branch_vec
                         {
                             let _ = octocrab
